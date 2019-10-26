004354d01fee5661a6c0477bd188d5caf88532ae
HristinaSotirova4342 <56221929+HristinaSotirova4342@users.noreply.github.com>
2019-10-26 11:52:54 +0200

    Add files via upload

---
+++ homework.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace homework._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+
+            for(int i = 1; i <= number; i++)
+            {
+                if(number % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.Write("Prime number: " + number);
+            }
+            else
+            {
+                Console.Write("Not a Prime number: " + number);
+            }
+           
+
+            Console.ReadLine();
+        }
+    }
+}
