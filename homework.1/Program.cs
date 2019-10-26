70ce635291952b08fb1e0b761401417461d444c0
HristinaSotirova4342 <tina.sotirova99@gmail.com>
2019-10-26 11:50:01 +0200

    first commit

---
+++ homework.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace homework._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter a string: ");
+            string str = Console.ReadLine();
+
+            string revers;
+            revers = "";
+
+        
+            int length1;
+            length1 = str.Length - 1;
+
+            while (length1 >= 0)
+            {
+                revers += str[length1];
+                length1--;
+            }
+            Console.WriteLine("Reversed: {0}", revers);
+            Console.ReadLine();
+        }
+    }
+}
+ 
