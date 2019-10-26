cb55bbb2886c870f7003d918c4b9ae75574a1e92
HristinaSotirova4342 <56221929+HristinaSotirova4342@users.noreply.github.com>
2019-10-26 11:56:56 +0200

    Add files via upload

---
+++ hommework2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace hommework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int answer = 0;
+            while (answer <= 10)
+            {
+                Console.Write("Enter an integer: ");
+                answer = Convert.ToInt32(Console.ReadLine());
+
+                if (!(answer <= 10))
+                {
+                    Console.WriteLine("Integer greter than 10 detected!");
+                }
+             
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
