e77683bc119d36367879da39c26688d6b8e67943
HristinaSotirova4342 <56221929+HristinaSotirova4342@users.noreply.github.com>
2019-10-30 10:46:21 +0100

    Add files via upload

---
+++ Homework.3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Homework._3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num1 + num2;
+            Console.Write("The sum of two numbers is : {0} \n", sum);
+
+            string binary = Convert.ToString(sum, 2);
+            Console.Write("The binary sum of two numbers is: {0}\n", binary);
+
+
+        }
+    }
+}
