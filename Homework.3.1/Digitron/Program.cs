9251f6f4c18ed931f17b959ecf6a7bc4b75b6edd
HristinaSotirova4342 <56221929+HristinaSotirova4342@users.noreply.github.com>
2019-10-30 10:44:10 +0100

    Add files via upload

---
+++ Homework.3.1/Digitron/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Digitron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operatot: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.Write(num1 + num2);
+            }
+            else if(op == "-"){
+                Console.Write(num1 - num2);
+            }
+            else if (op == "*"){
+                Console.Write(num1 * num2);
+            }
+            else if (op == "/"){
+                Console.Write(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
